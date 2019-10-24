20f9c0e69105a09e499c3f89f31eb768ae21b72b
Iago Vitoriano <iago.bezerra@restoque.com.br>
2019-10-24 17:15:53 -0300

    example

---
+++ example.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mercadopago = require("mercadopago");
+
+const app = express();
+
+app.use("/payment", async (req, res) => {
+  mercadopago.configurations.setAccessToken(config.access_token);
+
+  var payment_data = {
+    transaction_amount: 169,
+    token: "ff8080814c11e237014c1ff593b57b4d",
+    description: "Aerodynamic Copper Plate",
+    installments: 1,
+    payment_method_id: "visa",
+    payer: {
+      email: "alva@hotmail.com"
+    }
+  };
+
+  mercadopago.payment
+    .save(payment_data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+});
+
+app.listen(3000, () => console.log("running!!!"));
